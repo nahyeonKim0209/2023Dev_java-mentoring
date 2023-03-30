3c22a3b47d125057e81bcf063812931ac0971477
nahyeonKim0209 <117059665+nahyeonKim0209@users.noreply.github.com>
2023-03-30 19:19:41 +0900

    docs: 기능-입력 추가

--- src/com/iKeeper/Day1/Calculate.java
+++ src/com/iKeeper/Day1/Calculate.java
@@ -1,4 +1,7 @@
 package com.iKeeper.Day1;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Calculate {
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 }
